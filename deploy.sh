269fec4e6cb3c598fa175caa0c8e53302e4d3551
DeVoresyah ArEst <de.voresyah@gmail.com>
2021-07-04 16:31:47 +0700

    config: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build npm
+yarn build
+
+# add commit
+git add build
+
+echo "Commit message for gh-pages commit?"
+read pagecommit
+
+git commit -m "deploy: $pagecommit"
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git subtree push --prefix dist origin gh-pages
+#git push origin `git subtree split --prefix dist master`:gh-pages --force
