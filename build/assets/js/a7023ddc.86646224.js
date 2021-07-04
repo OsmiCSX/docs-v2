05e6223fb1447831458f0f7e64b693ea8347835e
DeVoresyah ArEst <de.voresyah@gmail.com>
2021-07-04 16:34:28 +0700

    deploy: initial release

---
+++ build/assets/js/a7023ddc.86646224.js
@@ -0,0 +1,1 @@
+(self.webpackChunkosmicsx=self.webpackChunkosmicsx||[]).push([[1713],{7485:function(n){"use strict";n.exports=JSON.parse('{"announcement":{"allTagsPath":"/blog/tags","slug":"announcement","name":"announcement","count":1,"permalink":"/blog/tags/announcement"}}')}}]);
