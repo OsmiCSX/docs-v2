a40882ed3e7187d60ff8be8604042a2e8f26b2b4
Rully Ardiansyah <rully.ardiansyah@kuncicoin.com>
2022-07-21 20:17:10 +0700

    deploy: add docs for notch, and specific platform styling

--- assets/js/814f3328.2838bada.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkosmicsx=self.webpackChunkosmicsx||[]).push([[2535],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Switch Theme Engine","permalink":"/blog/2021/07/07/switch-theme-engine"},{"title":"Official Release v0.6","permalink":"/blog/2021/07/04/official-release-v0-6"}]}')}}]);
