c989272db1c60d9ddf61cdaa60d038b03e5d6786
Ikhsan <dev.ikhsanmohamad@gmail.com>
2022-10-05 16:27:39 +0700

    feat: add what is osmi section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,106 +1,100 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'OsmiCSX',
-  tagline: 'An utility style framework for React Native',
-  url: 'https://osmicsx.github.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'osmicsx', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "OsmiCSX",
+  tagline: "An utility style framework for React Native",
+  url: "https://osmicsx.github.io/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "osmicsx", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'OsmiCSX',
       logo: {
-        alt: 'OsmiCSX',
-        src: 'img/logo.svg',
+        alt: "OsmiCSX",
+        src: "img/new-logo.svg",
       },
       items: [
         {
-          type: 'docsVersionDropdown',
-          position: 'left',
-          dropdownActiveClassDisabled: true,
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Docs",
         },
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'right',
-          label: 'Docs',
-        },
-        {to: '/blog', label: 'Blog', position: 'right'},
-        {
-          href: 'https://github.com/OsmiCSX/osmicsx',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/OsmiCSX/osmicsx",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
-    footer: {
-      style: 'dark',
-      // links: [
-      //   {
-      //     title: 'Docs',
-      //     items: [
-      //       {
-      //         label: 'Tutorial',
-      //         to: '/docs/intro',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'Community',
-      //     items: [
-      //       {
-      //         label: 'Stack Overflow',
-      //         href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //       },
-      //       {
-      //         label: 'Discord',
-      //         href: 'https://discordapp.com/invite/docusaurus',
-      //       },
-      //       {
-      //         label: 'Twitter',
-      //         href: 'https://twitter.com/docusaurus',
-      //       },
-      //     ],
-      //   },
-      //   {
-      //     title: 'More',
-      //     items: [
-      //       {
-      //         label: 'Blog',
-      //         to: '/blog',
-      //       },
-      //       {
-      //         label: 'GitHub',
-      //         href: 'https://github.com/facebook/docusaurus',
-      //       },
-      //     ],
-      //   },
-      // ],
-      copyright: `Copyright &copy; ${new Date().getFullYear()} OsmiCSX. Built with Docusaurus.`,
-    },
+    // footer: {
+    //   style: 'dark',
+    //   links: [
+    //     {
+    //       title: 'Docs',
+    //       items: [
+    //         {
+    //           label: 'Tutorial',
+    //           to: '/docs/intro',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'Community',
+    //       items: [
+    //         {
+    //           label: 'Stack Overflow',
+    //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+    //         },
+    //         {
+    //           label: 'Discord',
+    //           href: 'https://discordapp.com/invite/docusaurus',
+    //         },
+    //         {
+    //           label: 'Twitter',
+    //           href: 'https://twitter.com/docusaurus',
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       title: 'More',
+    //       items: [
+    //         {
+    //           label: 'Blog',
+    //           to: '/blog',
+    //         },
+    //         {
+    //           label: 'GitHub',
+    //           href: 'https://github.com/facebook/docusaurus',
+    //         },
+    //       ],
+    //     },
+    //   ],
+    //   copyright: `Copyright &copy; ${new Date().getFullYear()} OsmiCSX. Built with Docusaurus.`,
+    // },
     algolia: {
-      apiKey: '93e4892f05bb45f45fe60af0a02c950c',
-      appId: '6SLKHSIPOA',
-      indexName: 'dev_osmicsx',
+      apiKey: "93e4892f05bb45f45fe60af0a02c950c",
+      appId: "6SLKHSIPOA",
+      indexName: "dev_osmicsx",
       contextualSearch: true,
     },
     metadatas: [
       {
-        property: 'og:image',
-        content: 'https://osmicsx.dev/img/docusaurus.png',
+        property: "og:image",
+        content: "https://osmicsx.dev/img/docusaurus.png",
       },
-      {name: 'twitter:card', content: 'summary_large_image'},
+      { name: "twitter:card", content: "summary_large_image" },
       {
-        name: 'twitter:image',
-        content: 'https://osmicsx.dev/img/docusaurus.png',
+        name: "twitter:image",
+        content: "https://osmicsx.dev/img/docusaurus.png",
       },
-      {name: 'twitter:site', content: '@osmicsx'},
+      { name: "twitter:site", content: "@osmicsx" },
     ],
     prism: {
       theme: lightCodeTheme,
@@ -109,20 +103,19 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
           showLastUpdateAuthor: false,
           showLastUpdateTime: true,
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/OsmiCSX/docs-v2',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/OsmiCSX/docs-v2",
         },
         blog: {
           showReadingTime: true,
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,23 @@
-import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import HomepageWhatIsOsmi from "../components/HomepageWhatIsOsmi.jsx";
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             Getting Started 🚀
           </Link>
         </div>
@@ -26,14 +27,15 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Documentation`}
-      description="OsmiCSX - An utility style framework for React Native">
-      <HomepageHeader />
+      description="OsmiCSX - An utility style framework for React Native"
+    >
       <main>
-        <HomepageFeatures />
+        {/* What is Osmi Section */}
+        <HomepageWhatIsOsmi />
       </main>
     </Layout>
   );
